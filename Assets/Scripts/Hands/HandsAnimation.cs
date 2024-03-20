bfb77093b04102de2bfca45c25ad4a591a61bfc7
Shino974 <theo.turpin@epitech.eu>
2024-03-20 15:45:53 +0900

    Add: all basics file + basic scene + some assets

---
+++ Assets/Scripts/Hands/HandsAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandsAnimation : MonoBehaviour
+{
+    [SerializeField] private InputActionReference gripReference;
+    [SerializeField] private InputActionReference triggerReference;
+    [SerializeField] private Animator handAnimator;
+    void Update()
+    {
+        float gripValue = gripReference.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+        float triggerValue = triggerReference.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+    }
+}

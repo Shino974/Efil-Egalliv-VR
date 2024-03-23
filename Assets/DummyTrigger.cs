2c92c2f7b912f8bf6e9724b35f9e0e311846d1a9
Shino974 <theo.turpin@epitech.eu>
2024-03-23 13:37:43 +0900

    Add: Basic gameplay
    dummy can pop with preasure plate and die if touch with revolver, water gun or sword

---
+++ Assets/DummyTrigger.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject[] targetDummies;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (GameObject dummy in targetDummies)
+            {
+                dummy.GetComponent<TargetDummyBasics>().ActivateDummy();
+            }
+        }
+    }
+}

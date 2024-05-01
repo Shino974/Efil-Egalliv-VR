fdfee549a0d10d36e2345bd748b8102d6bad4737
Shino974 <theo.turpin@epitech.eu>
2024-05-01 19:00:52 +0900

    add: voices
    add voice for the prisonier and the merchant

--- Assets/Scripts/Musics & Sounds/MusicTrigger.cs
+++ Assets/Scripts/Musics & Sounds/MusicTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ public class MusicTrigger : MonoBehaviour
 {
     public AudioSource audioSource;
     public bool isLoop;
+    private bool isPlaying = false;
     private bool hasPlayed = false;
 
     private void Start()
@@ -15,11 +17,23 @@ public class MusicTrigger : MonoBehaviour
             audioSource.loop = false;
     }
 
+    private void Update()
+    {
+        if (!audioSource.isPlaying && isPlaying)
+        {
+            isPlaying = false;
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !hasPlayed) // Vérifier si le joueur entre en collision avec le cube et si la musique n'a pas encore été jouée
         {
-            audioSource.Play();
+            if (!isPlaying)
+            {
+                audioSource.Play();
+                isPlaying = true;
+            }
             if (isLoop)
                 hasPlayed = true;
             else
